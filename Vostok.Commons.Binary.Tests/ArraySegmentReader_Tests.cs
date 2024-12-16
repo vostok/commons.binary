83ba2aca9fcd661f3351c8fe196399851daa353d
deniaa <deniaa@skbkontur.ru>
2024-12-16 10:49:57 +0500

    We can't use modern language features right now

--- Vostok.Commons.Binary.Tests/ArraySegmentReader_Tests.cs
+++ Vostok.Commons.Binary.Tests/ArraySegmentReader_Tests.cs
@@ -2,36 +2,37 @@ using System;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace Vostok.Commons.Binary.Tests;
-
-[TestFixture]
-public class ArraySegmentReader_Tests
+namespace Vostok.Commons.Binary.Tests
 {
-    [Test]
-    public void Test_bound_check()
+    [TestFixture]
+    public class ArraySegmentReader_Tests
     {
-        var segmentReader = new ArraySegmentReader(new ArraySegment<byte>(new byte[42], 14, 4));
-        segmentReader.Position.Should().Be(0);
-        segmentReader.BytesRemaining.Should().Be(sizeof(int));
-        segmentReader.ReadInt32().Should().Be(0);
-        segmentReader.Position.Should().Be(sizeof(int));
-        segmentReader.BytesRemaining.Should().Be(0);
+        [Test]
+        public void Test_bound_check()
+        {
+            var segmentReader = new ArraySegmentReader(new ArraySegment<byte>(new byte[42], 14, 4));
+            segmentReader.Position.Should().Be(0);
+            segmentReader.BytesRemaining.Should().Be(sizeof(int));
+            segmentReader.ReadInt32().Should().Be(0);
+            segmentReader.Position.Should().Be(sizeof(int));
+            segmentReader.BytesRemaining.Should().Be(0);
 
-        new Action(() => segmentReader.ReadBool()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadByte()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadDouble()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadFloat()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadGuid()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadInt16()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadInt32()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadInt64()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadUInt16()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadUInt32()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadUInt64()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadString()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadShortString()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadByteArray()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadVarlenUInt32()).Should().Throw<IndexOutOfRangeException>();
-        new Action(() => segmentReader.ReadVarlenUInt64()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadBool()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadByte()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadDouble()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadFloat()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadGuid()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadInt16()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadInt32()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadInt64()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadUInt16()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadUInt32()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadUInt64()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadString()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadShortString()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadByteArray()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadVarlenUInt32()).Should().Throw<IndexOutOfRangeException>();
+            new Action(() => segmentReader.ReadVarlenUInt64()).Should().Throw<IndexOutOfRangeException>();
+        }
     }
 }
--- Vostok.Commons.Binary/ArraySegmentReader.cs
+++ Vostok.Commons.Binary/ArraySegmentReader.cs
@@ -3,340 +3,341 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using JetBrains.Annotations;
 
-namespace Vostok.Commons.Binary;
-
-/// <summary>
-/// <para>An implementation of <see cref="IBinaryReader"/> based on an in-memory byte array.</para>
-/// <para>Supports changing <see cref="Position"/> and <see cref="Endianness"/> on the fly.</para>
-/// <para>Not thread-safe.</para>
-/// </summary>
-[PublicAPI]
-internal class ArraySegmentReader : IBinaryReader
+namespace Vostok.Commons.Binary
 {
-    private int position;
-    
-    public ArraySegmentReader(ArraySegment<byte> segment)
+    /// <summary>
+    /// <para>An implementation of <see cref="IBinaryReader"/> based on an in-memory byte array.</para>
+    /// <para>Supports changing <see cref="Position"/> and <see cref="Endianness"/> on the fly.</para>
+    /// <para>Not thread-safe.</para>
+    /// </summary>
+    [PublicAPI]
+    internal class ArraySegmentReader : IBinaryReader
     {
-        if (segment.Array == null) 
-            throw new ArgumentNullException(nameof(segment), "ArraySegment<byte> contains null array");
-        Segment = segment;
-        position = 0; 
-    }
+        private int position;
 
-    public ArraySegment<byte> Segment { get; }
-
-    public long Position
-    {
-        get => position;
-        set
+        public ArraySegmentReader(ArraySegment<byte> segment)
         {
-            if (value > int.MaxValue)
-                throw new ArgumentOutOfRangeException(nameof(value), $"{nameof(ArraySegmentReader)} does not support positions greater than int.MaxValue.");
-            position = (int)value;
+            if (segment.Array == null)
+                throw new ArgumentNullException(nameof(segment), "ArraySegment<byte> contains null array");
+            Segment = segment;
+            position = 0;
         }
-    }
 
-    public long BytesRemaining => Segment.Count - position;
+        public ArraySegment<byte> Segment { get; }
 
-    public Endianness Endianness { get; set; } = EndiannessConverter.SystemEndianness;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public byte ReadByte()
-    {
-        const int ValueSize = sizeof(byte);
-        
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        var result = Segment.Array![Segment.Offset + position];
-
-        position += ValueSize;
-
-        return result;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool ReadBool()
-    {
-        const int ValueSize = sizeof(byte);
-        
-        EnsureSufficientSizeRemaining(ValueSize);
-        
-        var result = Segment.Array![Segment.Offset + position] != 0;
-
-        position += ValueSize;
-
-        return result;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe short ReadInt16()
-    {
-        const int ValueSize = sizeof(short);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        short result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(short*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe int ReadInt32()
-    {
-        const int ValueSize = sizeof(int);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        int result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(int*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe long ReadInt64()
-    {
-        const int ValueSize = sizeof(long);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        long result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(long*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe ushort ReadUInt16()
-    {
-        const int ValueSize = sizeof(ushort);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        ushort result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(ushort*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe uint ReadUInt32()
-    {
-        const int ValueSize = sizeof(uint);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        uint result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(uint*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe ulong ReadUInt64()
-    {
-        const int ValueSize = sizeof(ulong);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        ulong result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(ulong*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe float ReadFloat()
-    {
-        const int ValueSize = sizeof(float);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        float result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(float*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe double ReadDouble()
-    {
-        const int ValueSize = sizeof(double);
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        double result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(double*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe Guid ReadGuid()
-    {
-        const int ValueSize = 16;
-
-        EnsureSufficientSizeRemaining(ValueSize);
-
-        Guid result;
-
-        fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
-            result = *(Guid*)ptr;
-
-        position += ValueSize;
-
-        return EndiannessConverter.Convert(result, Endianness);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public uint ReadVarlenUInt32()
-    {
-        var result = 0U;
-        var shift = 0;
-        var size = 0;
-        var hasMore = true;
-
-        while (hasMore)
+        public long Position
         {
-            EnsureSufficientSizeRemaining(size + 1);
-            
-            var currentByte = Segment.Array![Segment.Offset + position + size];
-
-            if (size == sizeof(uint))
+            get => position;
+            set
             {
-                result |= (uint)currentByte << shift;
+                if (value > int.MaxValue)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"{nameof(ArraySegmentReader)} does not support positions greater than int.MaxValue.");
+                position = (int)value;
+            }
+        }
+
+        public long BytesRemaining => Segment.Count - position;
+
+        public Endianness Endianness { get; set; } = EndiannessConverter.SystemEndianness;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte ReadByte()
+        {
+            const int ValueSize = sizeof(byte);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            var result = Segment.Array![Segment.Offset + position];
+
+            position += ValueSize;
+
+            return result;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool ReadBool()
+        {
+            const int ValueSize = sizeof(byte);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            var result = Segment.Array![Segment.Offset + position] != 0;
+
+            position += ValueSize;
+
+            return result;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe short ReadInt16()
+        {
+            const int ValueSize = sizeof(short);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            short result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(short*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe int ReadInt32()
+        {
+            const int ValueSize = sizeof(int);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            int result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(int*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe long ReadInt64()
+        {
+            const int ValueSize = sizeof(long);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            long result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(long*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe ushort ReadUInt16()
+        {
+            const int ValueSize = sizeof(ushort);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            ushort result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(ushort*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe uint ReadUInt32()
+        {
+            const int ValueSize = sizeof(uint);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            uint result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(uint*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe ulong ReadUInt64()
+        {
+            const int ValueSize = sizeof(ulong);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            ulong result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(ulong*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe float ReadFloat()
+        {
+            const int ValueSize = sizeof(float);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            float result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(float*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe double ReadDouble()
+        {
+            const int ValueSize = sizeof(double);
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            double result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(double*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe Guid ReadGuid()
+        {
+            const int ValueSize = 16;
+
+            EnsureSufficientSizeRemaining(ValueSize);
+
+            Guid result;
+
+            fixed (byte* ptr = &Segment.Array![Segment.Offset + position])
+                result = *(Guid*)ptr;
+
+            position += ValueSize;
+
+            return EndiannessConverter.Convert(result, Endianness);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public uint ReadVarlenUInt32()
+        {
+            var result = 0U;
+            var shift = 0;
+            var size = 0;
+            var hasMore = true;
+
+            while (hasMore)
+            {
+                EnsureSufficientSizeRemaining(size + 1);
+
+                var currentByte = Segment.Array![Segment.Offset + position + size];
+
+                if (size == sizeof(uint))
+                {
+                    result |= (uint)currentByte << shift;
+                    size++;
+                    break;
+                }
+
+                hasMore = (currentByte & 0x80) != 0;
+                result |= (uint)(currentByte & 0x7F) << shift;
+                shift += 7;
                 size++;
-                break;
             }
 
-            hasMore = (currentByte & 0x80) != 0;
-            result |= (uint)(currentByte & 0x7F) << shift;
-            shift += 7;
-            size++;
+            position += size;
+
+            return result;
         }
 
-        position += size;
-
-        return result;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ulong ReadVarlenUInt64()
-    {
-        var result = 0UL;
-        var shift = 0;
-        var size = 0;
-        var hasMore = true;
-
-        while (hasMore)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ulong ReadVarlenUInt64()
         {
-            EnsureSufficientSizeRemaining(size + 1);
-            
-            var currentByte = Segment.Array![Segment.Offset + position + size];
+            var result = 0UL;
+            var shift = 0;
+            var size = 0;
+            var hasMore = true;
 
-            if (size == sizeof(ulong))
+            while (hasMore)
             {
-                result |= (ulong)currentByte << shift;
+                EnsureSufficientSizeRemaining(size + 1);
+
+                var currentByte = Segment.Array![Segment.Offset + position + size];
+
+                if (size == sizeof(ulong))
+                {
+                    result |= (ulong)currentByte << shift;
+                    size++;
+                    break;
+                }
+
+                hasMore = (currentByte & 0x80) != 0;
+                result |= (ulong)(currentByte & 0x7F) << shift;
+                shift += 7;
                 size++;
-                break;
             }
 
-            hasMore = (currentByte & 0x80) != 0;
-            result |= (ulong)(currentByte & 0x7F) << shift;
-            shift += 7;
-            size++;
+            position += size;
+
+            return result;
         }
 
-        position += size;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public virtual string ReadString(Encoding encoding)
+        {
+            var size = ReadInt32();
 
-        return result;
-    }
+            EnsureSufficientSizeRemaining(size);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public virtual string ReadString(Encoding encoding)
-    {
-        var size = ReadInt32();
+            var result = encoding.GetString(Segment.Array!, Segment.Offset + position, size);
 
-        EnsureSufficientSizeRemaining(size);
+            position += size;
 
-        var result = encoding.GetString(Segment.Array!, Segment.Offset + position, size);
+            return result;
+        }
 
-        position += size;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public virtual string ReadShortString(Encoding encoding)
+        {
+            var size = ReadByte();
 
-        return result;
-    }
+            EnsureSufficientSizeRemaining(size);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public virtual string ReadShortString(Encoding encoding)
-    {
-        var size = ReadByte();
+            var result = encoding.GetString(Segment.Array!, Segment.Offset + position, size);
 
-        EnsureSufficientSizeRemaining(size);
+            position += size;
 
-        var result = encoding.GetString(Segment.Array!, Segment.Offset + position, size);
+            return result;
+        }
 
-        position += size;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte[] ReadByteArray()
+        {
+            return ReadByteArray(ReadInt32());
+        }
 
-        return result;
-    }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte[] ReadByteArray(int size)
+        {
+            EnsureSufficientSizeRemaining(size);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public byte[] ReadByteArray()
-    {
-        return ReadByteArray(ReadInt32());
-    }
+            var result = new byte[size];
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public byte[] ReadByteArray(int size)
-    {
-        EnsureSufficientSizeRemaining(size);
+            Buffer.BlockCopy(Segment.Array!, Segment.Offset + position, result, 0, size);
 
-        var result = new byte[size];
+            position += size;
 
-        Buffer.BlockCopy(Segment.Array!, Segment.Offset + position, result, 0, size);
+            return result;
+        }
 
-        position += size;
-
-        return result;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void EnsureSufficientSizeRemaining(int size)
-    {
-        if (size > BytesRemaining)
-            throw new IndexOutOfRangeException($"Requested to read {size} bytes from buffer, but it only has {BytesRemaining} bytes remaining.");
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void EnsureSufficientSizeRemaining(int size)
+        {
+            if (size > BytesRemaining)
+                throw new IndexOutOfRangeException($"Requested to read {size} bytes from buffer, but it only has {BytesRemaining} bytes remaining.");
+        }
     }
 }
